5d27d6219005f3ca2f5d8efb4f6e90846eb9cb91
xliuzzx <115203133+xliuzzx@users.noreply.github.com>
2024-03-06 14:35:45 +0700

    Add files via upload

---
+++ Praktikum1/app/src/main/java/com/ArliusBarasa/helloapss/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ArliusBarasa.helloapss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
